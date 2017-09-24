074a3c3c5c6688d552d9f051c1b452af49ad30f4
Unknown <clarahtian@gmail.com>
2017-09-23 21:46:00 -0500

    Auto stash before merge of "master" and "origin/master"

--- hello/views.py
+++ hello/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
-import requests
 import os
 
 # Create your views here.
 def index(request):
     times = int(os.environ.get('TIMES',3))
-    return render(request, 'index.html')
+    return render(request, 'images.html')
     #return HttpResponse('hello world')
 
 
